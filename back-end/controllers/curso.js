572c87da7df6214b3e39aedcf9cf8b5e155495b8
PamelaOsorio <pmosorio84@gmail.com>
2020-09-02 14:11:00 +0000

    (02/09) Primeiro model

---
+++ back-end/controllers/curso.js
@@ -0,0 +1,26 @@
+/*
+
+ QUATRO OPERAÇÕES BÁSICAS SOBRE DADOS
+
+ 1) CREATE (criação ou inserção)
+     Cria um novo objeto dentro da coleção
+
+2) RETRIEVE (recuperação ou listragem)
+    Permite recuperar os dados a partir do BD
+
+3) UPDATE (atualizaçaõ)
+     Altera  os dados de um objeto que JÁ EXISTE no BD
+
+4)DELETE (exclusão)
+     Elimina um objeto do BD 
+
+     (C)reate + (R)etrieve + (U)pdate + (D)elete = CRUD
+
+ VERBOS HTTP ASSOCIADOS ÁS OPERAÇÕES CRUD
+
+ Verbo  Operação
+ POST   Create 
+ GET    Retrieve
+ PUT    Update
+ DELETE Delete
+*/
