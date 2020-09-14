8cc568301b2bf9d362dc2274f38f865c2ef7cfbd
PamelaOsorio <pmosorio84@gmail.com>
2020-09-14 14:08:00 +0000

    (14/09)  Método

--- back-end/controllers/curso.js
+++ back-end/controllers/curso.js
@@ -30,6 +30,7 @@ const controller = {} // Objeto vazio
 
 // Métodos novo(), implementando a operação CREATE
 controller.novo = async (req, res) => {
+
 try{
     // Envia os dados dentro de req.body para o BD para criação
     await Curso.create(req.body)
@@ -40,5 +41,30 @@ try{
       // HTTP 500: Internal Server Error
       res.status(500).send(erro)
   }
+}
+
+// Método listar(), implementsndo a operação RETRIEVE (all)
+controller.listar = async (req, res) => {
+    try{
+    // find() sem parâmetros é trazer tudo
+    let dados = await Curso.find()
+    res.send(dados)
+    }
+    catch(erro) {
+        console.error(erro)
+        res.status(500).send(erro)
+    }
+}
+
+// Método obterUm() , implementando a operação RETRIEVE (one)
+controller.obterUm = async (req, res) => {
+    const id = req.params.id // Capturando o parâmetro id
+    let obj = await Curso.findById(id)
+
+
+    // Se o objeto vier preenchido (achou), então o retornamos
+    if(obj) res.send(obj)
+    // Senão (objeto vazio), enviamos o status HTTP  404: Not found
+    else res.status(404).end()
 }
 module.exports = controller
--- back-end/routes/curso.js
+++ back-end/routes/curso.js
@@ -4,5 +4,5 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/', controller.novo) //Create
-
+router.get('/', controller.listar) //Retrieve
 module.exports = router
